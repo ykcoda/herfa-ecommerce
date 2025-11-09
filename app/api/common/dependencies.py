c37ef377c295d601b5d67bcbac4d26d2b2be1f24
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-09 18:23:08 +0000

    Add a USER_SERVICE_DEP

--- app/api/common/dependencies.py
+++ app/api/common/dependencies.py
@@ -2,7 +2,17 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.database.db_session import get_db_async_session
+from app.api.service.user_service import UserService
 
 
-# Creates a dependency for Database session
+# Database Session Dependency
 DB_SESSION_DEP = Annotated[AsyncSession, Depends(get_db_async_session)]
+
+
+# create a UserServiceDependant
+async def get_user_service(session: DB_SESSION_DEP):
+    return UserService(session)
+
+
+# User Service Dependency
+USER_SERVICE_DEP = Annotated[UserService, Depends(get_user_service)]
