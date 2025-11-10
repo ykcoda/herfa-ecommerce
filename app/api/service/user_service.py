da13bc401d22e48ae6ab23a7c03926d53bc1a3ff
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-10 06:31:22 +0000

    Update user_service to include logs, add a get_all_users query and update details for some raised http exceptions

--- app/api/service/user_service.py
+++ app/api/service/user_service.py
@@ -6,6 +6,7 @@ from app.api.service.base_service import BaseService
 from app.database.schemas.user import UserCreate, UserUpdate
 from datetime import datetime
 from uuid import UUID
+from app.utils import logging_service, LogType, LogServiceType
 
 from pwdlib import PasswordHash
 
@@ -24,6 +25,10 @@ class UserService(BaseService):
         result = await self.session.exec(statement)
         return result.first()
 
+    # get all users
+    async def get_all_user(self):
+        return await self._get_all()
+
     # create a user
     async def create_user(self, user: UserCreate):
         # check if email exists in uses table
@@ -32,11 +37,23 @@ class UserService(BaseService):
             new_user = User(
                 **user.model_dump(), password_hash=password_hash.hash(user.password)
             )
+            # log
+            logging_service.set_log(
+                f"Registered a new user with email {user.email}",
+                log_type=LogType.INFO,
+                log_service_type=LogServiceType.DATABASE,
+            )
             return await self._create(new_user)
         else:
+            # log
+            logging_service.set_log(
+                f"User with this email({user.email}) already exists",
+                log_type=LogType.ERROR,
+                log_service_type=LogServiceType.DATABASE,
+            )
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail="User with this email already exists",
+                detail=f"User with this email({user.email}) already exists",
             )
 
     # update a user
@@ -44,13 +61,27 @@ class UserService(BaseService):
         # get user by id
         user = await self._get(id)
         if not user:
+            # log
+            logging_service.set_log(
+                message=f"User with id '{id}' doen't exist",
+                log_type=LogType.ERROR,
+                log_service_type=LogServiceType.DATABASE,
+            )
             # raise and exception if user is not found
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="User doesn't exists!"
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"User with id '{id}' doen't exist",
             )
 
         # update users updated_at to current datetime
         user.updated_at = datetime.now()
+
+        # log
+        logging_service.set_log(
+            message=f"User with id '{id}' records [[{user_data.model_dump(exclude_none=True)}]] has been updated.",
+            log_type=LogType.ERROR,
+            log_service_type=LogServiceType.DATABASE,
+        )
 
         # update the user with updated data
         return await self._update(
@@ -62,11 +93,28 @@ class UserService(BaseService):
         # get user by id
         user = await self._get(id)
         if not user:
+            # log
+            logging_service.set_log(
+                message=f"User with id '{id}' doen't exist",
+                log_type=LogType.ERROR,
+                log_service_type=LogServiceType.DATABASE,
+            )
             # raise and exception if user is not found
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="User doesn't exists!"
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"User with id '{id}' doen't exist",
             )
+
+        # update users deleted attribure to true
+        user.deleted = True
+        user.updated_at = datetime.now()
         # delete user
         await self._delete(user)
 
+        # log
+        logging_service.set_log(
+            message=f"User with id '{id}' has been deleted.",
+            log_type=LogType.INFO,
+            log_service_type=LogServiceType.DATABASE,
+        )
         return {"detail": "User deleted!"}
