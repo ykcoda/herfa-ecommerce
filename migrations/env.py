b37bee859fc40fcb25efa95e785660cb6f8dab87
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-10 12:22:42 +0000

    Add an alembic migration for products and categories tables

--- migrations/env.py
+++ migrations/env.py
@@ -10,6 +10,8 @@ from alembic import context
 
 
 from app.database.model.user import User  # noqa
+from app.database.model.product import Product  # noqa
+from app.database.model.category import Category  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
