110dafdfe04f3a599c762514bc39e14f819678ba
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-10 11:41:57 +0000

    Capture logs when users hit the /token and /logout endpoints

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from app.database.schemas.user import UserCreate, UserRead, UserUpdate
 from app.api.common.dependencies import USER_SERVICE_DEP, AUTHENTICATED_USER_DEP
+from app.utils import logging_service, LogServiceType, LogType, redis_service
 from uuid import UUID
 from typing import Annotated
-
 
 # create an endpoint router for users
 users = APIRouter(prefix="/api/users", tags=["Users"])
@@ -27,13 +27,22 @@ async def register_user(data: UserCreate, service: USER_SERVICE_DEP):
 
 # update a user
 @users.put("/update")
-async def update_user(id: UUID, data: UserUpdate, service: USER_SERVICE_DEP):
+async def update_user(
+    id: UUID,
+    data: UserUpdate,
+    service: USER_SERVICE_DEP,
+    authorized_user: AUTHENTICATED_USER_DEP,
+):
     return await service.update_user(id, data)
 
 
 # delete a user
 @users.delete("/delete")
-async def delete_user(id: UUID, service: USER_SERVICE_DEP):
+async def delete_user(
+    id: UUID,
+    service: USER_SERVICE_DEP,
+    authorized_user: AUTHENTICATED_USER_DEP,
+):
     return await service.delete_user(id)
 
 
@@ -43,4 +52,26 @@ async def login_user(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     service: USER_SERVICE_DEP,
 ):
-    return await service.login_user(form_data.username, form_data.password)
+    # request for user access token
+    token = await service.login_user(form_data.username, form_data.password)
+
+    if token:
+        # log
+        logging_service.set_log(f"{form_data.username} has successfully login..", log_service_type=LogServiceType.ENDPOINT, log_type=LogType.INFO)  # type: ignore
+
+        return {
+            "access_token": token,
+            "token_type": "jwt",
+        }
+
+
+# logout user
+@users.post("/logout")
+async def logout_user(authorized_user: AUTHENTICATED_USER_DEP):
+    if authorized_user:
+        # log
+        logging_service.set_log(f"{authorized_user['user']['email']} has logout..", log_service_type=LogServiceType.ENDPOINT, log_type=LogType.INFO)  # type: ignore
+
+        # save authorized users jti access token to redis
+        await redis_service.save_to_redis(authorized_user["jti"])  # type: ignore
+        return {"detail": "User has successfully logout."}
