2909d98f1bbf4c7c347634a2c417102fea637691
Yaw Kyereh-Afreh <yawkyereh@outlook.com>
2025-11-10 12:21:37 +0000

    Add a Category Model

--- app/database/model/category.py
+++ app/database/model/category.py
@@ -1,6 +1,10 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID, uuid4
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.database.model.product import Product
 
 
 class Category(SQLModel, table=True):
@@ -10,3 +14,8 @@ class Category(SQLModel, table=True):
     name: str
     description: str
     created_at: datetime = Field(default_factory=datetime.now)
+
+    # Relationship
+    products: list["Product"] = Relationship(
+        back_populates="category", sa_relationship_kwargs={"lazy": "selectin"}
+    )
